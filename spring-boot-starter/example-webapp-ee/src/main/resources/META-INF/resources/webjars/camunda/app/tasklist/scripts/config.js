38c0a3984cef6dc18a9bbd613779352a41372d15
Thorben Lindhauer <thorben.lindhauer@camunda.com>
2021-03-19 10:53:59 +0100

    demonstrate configuration of tasklist in Spring Boot project

---
+++ spring-boot-starter/example-webapp-ee/src/main/resources/META-INF/resources/webjars/camunda/app/tasklist/scripts/config.js
@@ -0,0 +1,30 @@
+window.camTasklistConf = {
+  app: {
+    name: 'SUPPORT-10000',
+    vendor: 'Your Vendor Name'
+  },
+
+  // default config
+  shortcuts: {
+    claimTask: {
+      key: 'ctrl+alt+c',
+      description: 'claims the currently selected task'
+    },
+    focusFilter: {
+      key: 'ctrl+shift+f',
+      description: 'set the focus to the first filter in the list'
+    },
+    focusList: {
+      key: 'ctrl+alt+l',
+      description: 'sets the focus to the first task in the list'
+    },
+    focusForm: {
+      key: 'ctrl+alt+f',
+      description: 'sets the focus to the first input field in a task form'
+    },
+    startProcess: {
+      key: 'ctrl+alt+p',
+      description: 'opens the start process modal dialog'
+    }
+  }
+};
